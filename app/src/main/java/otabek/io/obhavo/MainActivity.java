f48f9b2dfab1dc49b151d29bae584123890cf628
Otabek <abduraufov_otabek@mail.ru>
2020-05-16 18:20:39 +0500

    initial commit

---
+++ app/src/main/java/otabek/io/obhavo/MainActivity.java
@@ -0,0 +1,434 @@
+package otabek.io.obhavo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Objects;
+import java.util.Random;
+
+import cz.msebera.android.httpclient.Header;
+
+import static com.loopj.android.http.AsyncHttpClient.LOG_TAG;
+import static com.loopj.android.http.AsyncHttpClient.log;
+
+public class MainActivity extends AppCompatActivity {
+    String cityname;
+
+    final String TAG = "aka";
+    int latitude;
+    int longitude;
+    String API_KEY = "0674092fb1dd45d88c6da2b68b4f37c9";
+    String url = "https://api.weatherbit.io/v2.0/forecast/daily?";
+
+    private void iconSetter(int[] codes){
+        String[] iconNames = {" "," "," "," "," "," "," "};
+        for(int i =0; i <codes.length;i++){
+            if (codes[i] >=200 & codes[i] <= 233){
+                iconNames[i] = "ic_lightning";
+            }
+            if (codes[i] >=300 && codes[i] <= 522){
+                iconNames[i] = "ic_rainy";
+            }
+            if (codes[i] >=611 && codes[i] <= 612){
+                iconNames[i] = "ic_sleet";
+            }
+            if (codes[i] >=600 &&  codes[i] <= 623){
+                iconNames[i] = "ic_snowing";
+            }
+            if (codes[i] >=700 &&  codes[i] <= 751){
+                iconNames[i] = "ic_foggy";
+            }
+            if (codes[i] >=800 && codes[i] <803){
+                iconNames[i] = "ic_sunny";
+            }
+            if (codes[i] >=803 && codes[i] <= 804){
+                iconNames[i] = "ic_partcloudy";
+            }
+        }
+
+        ImageView[] icons = {findViewById(R.id.icon),
+                findViewById(R.id.icon1),
+                findViewById(R.id.icon2),
+                findViewById(R.id.icon3),
+                findViewById(R.id.icon4),
+                findViewById(R.id.icon5),
+                findViewById(R.id.icon6)
+        };
+//        icons[0] = findViewById(R.id.icon);
+//        icons[1] = findViewById(R.id.icon1);
+//        icons[2] = findViewById(R.id.icon2);
+//        icons[3] = findViewById(R.id.icon3);
+//        icons[4] = findViewById(R.id.icon4);
+//        icons[5] = findViewById(R.id.icon6);
+        for (int i =0; i<icons.length; i++){
+            int resId = getResources().getIdentifier(iconNames[i],"drawable",getPackageName());
+            icons[i].setImageResource(resId);
+        }
+        
+    }
+
+
+    private void tempSetter(int[] codes){
+        TextView[] weatherTemps ={
+                findViewById(R.id.temp),
+                findViewById(R.id.temp1),
+                findViewById(R.id.temp2),
+                findViewById(R.id.temp3),
+                findViewById(R.id.temp4),
+                findViewById(R.id.temp5),
+                findViewById(R.id.temp6),
+        };
+
+        for (int i = 0; i <codes.length; i++){
+            weatherTemps[i].setText(Integer.toString(codes[i])+"°");
+        }
+
+    }
+
+    private void isWindy(JSONObject response) throws JSONException {
+        int windSpeed = ((int) response.getJSONArray("data").getJSONObject(0).getDouble("wind_spd"));
+        if (windSpeed > 8){
+            updateBackgroundPhoto(999);
+        }
+
+    }
+    private void updateBackgroundPhoto(int weatherCode){
+        String backPhotoName = null;
+
+
+        Random rand = new Random();
+        String  randomNumber = String.valueOf(rand.nextInt(3));
+        if (weatherCode >=202 && weatherCode <= 522){
+            backPhotoName ="rain";
+        }
+        if (weatherCode >=800 && weatherCode <=804){
+            backPhotoName = "sunny";
+        }
+        if (weatherCode >=600 && weatherCode <=623){
+            backPhotoName = "snow";
+        }
+        if (weatherCode >=700 && weatherCode <=751){
+            backPhotoName ="foggy";
+        }
+        if (weatherCode == 999){
+            backPhotoName = "wind"+randomNumber;
+        }
+
+        int te = getResources().getIdentifier(backPhotoName,"drawable",getPackageName());
+        ConstraintLayout constraintLayout = findViewById(R.id.main);
+        constraintLayout.setBackgroundResource(te);
+
+
+        }
+
+    public void jsonParser(JSONObject response) throws JSONException {
+        TextView cityNameView = findViewById(R.id.cityName);
+        cityNameView.setText(cityname);
+        String cityNameFromJson = response.getString("city_name");
+        cityNameView.setText(cityNameFromJson);
+
+        int[] codes={0,0,0,0,0,0,0};
+        int[] weatherDegrees ={0,0,0,0,0,0,0};
+        int todays_code = response.getJSONArray("data").getJSONObject(0).getJSONObject("weather").getInt("code");
+        codes[0] = todays_code;
+        updateBackgroundPhoto(todays_code);
+        for (int i =1; i <7; i++){
+            codes[i] = response.getJSONArray("data").getJSONObject(i).getJSONObject("weather").getInt("code");
+            weatherDegrees[i] = (int) response.getJSONArray("data").getJSONObject(i).getDouble("temp");
+        }
+        Log.i(TAG, "jsonParser: "+ Arrays.toString(codes));
+        iconSetter(codes);
+        tempSetter(weatherDegrees);
+
+
+    }
+
+
+
+
+
+
+    private void byCityName(String cityNamePassed){
+//        TextView cityNameView = findViewById(R.id.cityName);
+//        cityNameView.setText(cityNamePassed);
+        RequestParams params = new RequestParams();
+        params.put("city",cityNamePassed);
+        params.put("key",API_KEY);
+        Log.i(TAG, "byCityName passed: "+cityNamePassed);
+        hasActiveInternetConnection();
+        getWeatherInfo(params);
+
+    }
+
+    private void byCordinates(int lat, int lon){
+        RequestParams params = new RequestParams();
+        params.put("lat",lat);
+        params.put("lon",lon);
+        params.put("days",7);
+        params.put("key",API_KEY);
+        hasActiveInternetConnection();
+        getWeatherInfo(params);
+        Log.i(TAG, "byCordinates got: "+lat+" "+lon);
+    }
+    public void getWeatherInfo(RequestParams params){
+        Log.i(TAG, "getWeatherInfo: ");
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, params, new JsonHttpResponseHandler() {
+            
+            @Override
+            public void onStart() {
+                // called before request is started
+                Log.i(TAG, "onStart: ");
+            }
+
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // called when response HTTP status is "200 OK"
+//                Log.i(TAG, "onSuccess: "+response.toString());
+                try {
+                    jsonParser(response);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    int code = response.getJSONArray("data").getJSONObject(0).getJSONObject("weather").getInt("code");
+                    Log.i(TAG, "onSucces1s"+code);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),"Xatolik yuz berdi. Qayta urinib ko'ring!",Toast.LENGTH_SHORT).show();
+                    Log.i(TAG, "onSuccess2: "+e);
+                }
+            }
+
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
+            }
+        });
+
+
+
+
+    }
+
+
+    public  void hasActiveInternetConnection() {
+        AsyncHttpClient check = new AsyncHttpClient();
+        check.get("https://google.com", new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onStart() {
+                // called before request is started
+                Log.i(TAG, "onStart: ");
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                // called when response HTTP status is "200 OK"
+//                Log.i(TAG, "onSuccess: "+response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.i(TAG, "onFailure: "+statusCode+" "+errorResponse);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Internet mavjud emas")
+                        .setCancelable(false)
+                        .setPositiveButton("Wi Fi ni yoqish", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+                            }
+                        })
+                        .setNegativeButton("Bekor qilish", null );
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
+            }
+        });
+    }
+
+
+
+
+
+
+    public String Capitalize(String lowerCaseDayOfTheWeek) {
+        return lowerCaseDayOfTheWeek.substring(0, 1).toUpperCase() + lowerCaseDayOfTheWeek.substring(1).toLowerCase();
+    }
+
+
+    public String dayAdder(int numberOfdaysToadd) {
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
+        Date dt = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(dt);
+        c.add(Calendar.DATE, numberOfdaysToadd);
+        dt = c.getTime();
+
+
+        String dayOfTheWeek = sdf.format(dt);
+        return Capitalize(dayOfTheWeek);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+//        Log.i(TAG, "onResume: " + cityname);
+    }
+
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("city",cityname);
+        editor.putInt("lat",latitude);
+        editor.putInt("lon",longitude);
+        editor.commit();
+//        Log.i(TAG, "onPause: ");
+
+    }
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        Intent getIntent = getIntent();
+        cityname = getIntent.getStringExtra("city");
+        if (cityname == null){
+           latitude = getIntent.getIntExtra("lat",0);
+           longitude = getIntent.getIntExtra("long",0);
+            Log.i(TAG, "onCreate: "+latitude+" "+longitude);
+
+           if (latitude == 0 || longitude == 0){
+               SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+               cityname = sharedPref.getString("city","0");
+               latitude = sharedPref.getInt("lat",0);
+               longitude = sharedPref.getInt("lon",0);
+               if (latitude !=0){
+                   byCordinates(latitude,longitude);
+               }
+
+               if (cityname.equals("0") && latitude ==0 && longitude ==0){
+                   Intent sendIntent = new Intent(MainActivity.this,locationInput.class);
+                   MainActivity.this.startActivity(sendIntent);
+               }
+
+            } else{byCordinates(latitude,longitude);}
+            Log.i(TAG, "latitude "+latitude+" longitude"+longitude);
+
+        } else{byCityName(cityname);}
+
+
+//        byCityName(cityname);
+
+//            Log.i(TAG, "onCreate: " + cityname);
+
+
+        setContentView(R.layout.activity_main);
+
+
+        TextView cityName = findViewById(R.id.cityname);
+        TextView mainTemp = findViewById(R.id.temperature);
+
+        ImageButton changeCityButton = findViewById(R.id.changecity);
+
+//        hasActiveInternetConnection(this);
+        TextView cityNameView = findViewById(R.id.cityName);
+        TextView day = findViewById(R.id.day);
+        day.setText(dayAdder(0));
+        TextView day1 = findViewById(R.id.day1);
+        day1.setText(dayAdder(1));
+        TextView day2 = findViewById(R.id.day2);
+        day2.setText(dayAdder(2));
+        TextView day3 = findViewById(R.id.day3);
+        day3.setText(dayAdder(3));
+        TextView day4 = findViewById(R.id.day4);
+        day4.setText(dayAdder(4));
+        TextView day5 = findViewById(R.id.day5);
+        day5.setText(dayAdder(5));
+        TextView day6 = findViewById(R.id.day6);
+        day6.setText(dayAdder(6));
+
+        changeCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent(MainActivity.this,locationInput.class);
+                MainActivity.this.startActivity(sendIntent);
+            }
+        });
+
+
+        //Networking
+
+
+
+
+
+
+
+
+        }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+        if (keyCode == KeyEvent.KEYCODE_BACK ) {
+
+            this.finishAffinity();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+    }
+
---
+++ app/src/main/java/otabek/io/obhavo/locationInput.java
@@ -0,0 +1,237 @@
+package otabek.io.obhavo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+
+public class locationInput extends AppCompatActivity {
+    public int longitude;
+    public int latitude;
+    String message;
+    int counter ;
+    final String TAG = "aka";
+    LocationManager mLocationManager;
+    LocationListener mLocationListener;
+
+
+    boolean gps_enabled = false;
+    boolean network_enabled = false;
+
+
+
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+                return;
+            }
+
+        }
+    }
+
+    private void getWeatherForCurrentLocation() {
+
+        LocationManager lm = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch(Exception ex) {}
+
+//        try {
+//            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+//        } catch(Exception ex) {}
+
+        if(!gps_enabled && !network_enabled) {
+            counter = -1;
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("GPS Yoqilmagan");
+            builder.setPositiveButton("GPS sozlamalarni ochish", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    locationInput.this.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                }
+            })
+                    .setNegativeButton("Bekor qilish", null)
+                    .show();
+            Log.i(TAG, "onCreate: enable gps");
+            // notify user
+        }
+
+        mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+//                Log.i(TAG, "onLocationChanged: " + location.toString());
+                latitude = (int) location.getLatitude();
+                longitude = (int) location.getLongitude();
+                if (longitude != 0 && latitude !=0){
+                    Intent myIntent = new Intent(getBaseContext(),MainActivity.class);
+                    myIntent.putExtra("long",longitude);
+                    myIntent.putExtra("lat",latitude);
+                    Log.i(TAG, "onLocationChanged: "+latitude+" "+longitude);
+                    locationInput.this.startActivity(myIntent);
+
+                }
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 1000, mLocationListener);
+        }
+        if (counter == 0) {
+            message = "Shahar aniqlanmoqda. Bir oz sabr qiling . . .";
+        } else if (counter > 0){
+            message = "Sabr qil dedimku ey inson :-)";
+        }
+        if (counter != -1){
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location_input);
+
+
+
+        
+        
+        
+        
+
+        final EditText cityName = findViewById(R.id.cityname);
+        Button gpsFinder = findViewById(R.id.gps);
+        ImageButton swapButton = findViewById(R.id.swap);
+        swapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent(locationInput.this,MainActivity.class);
+                locationInput.this.startActivity(sendIntent);
+            }
+        });
+
+        gpsFinder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getWeatherForCurrentLocation();
+
+                counter ++;
+
+                Log.i(TAG, "onClick: "+latitude);
+                Log.i(TAG, "onClick: "+longitude);
+
+            }
+        });
+
+
+        cityName.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK ) {
+
+                    System.exit(0);
+                    return true;
+                }
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    String enteredCityName = cityName.getText().toString();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(cityName.getWindowToken(), 0);
+                    Log.i(TAG, "onKey: " + enteredCityName);
+                    Intent mIntent = new Intent(getApplicationContext(),MainActivity.class);
+                    mIntent.putExtra("city",enteredCityName);
+                    locationInput.this.startActivity(mIntent);
+                    return true;
+
+                }
+
+                return false;
+            }
+        });
+
+
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+        if (keyCode == KeyEvent.KEYCODE_BACK ) {
+            Log.i(TAG, "onKeyDown: ");
+          finishAffinity();
+          System.exit(0);
+
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
